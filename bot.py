0850d51a975a4d14924fcbf5e54662fe4c6f0373
egor5q <31898567+egor5q@users.noreply.github.com>
2018-07-12 00:04:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,10 @@ chats=db.chats
 def stats(m):
    x=chats.find_one({'id':m.from_user.id})
    if x!=None:
-      bot.send_message(m.chat.id, 'Статистика пользователя в данном чате:\nСообщения: '+str(x['users'][m.from_user.id]['messages'])+'\nМаты: '+str(x['users'][m.from_user.id]['mats']))
+      try:
+         bot.send_message(m.chat.id, 'Статистика пользователя в данном чате:\nСообщения: '+str(x['users'][m.from_user.id]['messages'])+'\nМаты: '+str(x['users'][m.from_user.id]['mats']))
+      except:
+         bot.send_message(m.chat.id, 'Вы еще не отправили в этот чат ни одного сообщения!')
    else:
       bot.send_message(m.chat.id, 'Вы ещё не отправили ни одного сообщения!')
 
