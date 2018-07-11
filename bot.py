2b71e0ab10b0368319a5ef4f7ad380eeeaf68e58
egor5q <31898567+egor5q@users.noreply.github.com>
2018-07-11 23:44:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,19 +26,49 @@ client=MongoClient(client1)
 db=client.grouphelper
 users=db.users
 
+@bot.message_handler(commands=['stat'])
+def stats(m):
+   x=users.find_one({'id':m.from_user.id})
+   if x!=None:
+      bot.send_message(m.chat.id, 'Статистика пользователя в данном чате:\nСообщения: '+str(x['messages'])+'\nМаты: '+str(x['mats']))
+   else:
+      bot.send_message(m.chat.id, 'Вы ещё не отправили ни одного сообщения!')
+
+
+
 @bot.message_handler(content_types=['text'])
 def mats(m):
+   chat=chats.find_one({'id':m.chat.id})
+   if chat==None:
+      chats.insert_one(createchat(m.chat.id))
+   if m.from_user.id not in chat['users']:
+      chats.update_one({'id':m.chat.id}, {'$set':{'users.'+str(m.from_user.id):createuser(m.from_user.id, m.from_user.username, m.from_user.first_name)}})
+   chats.update_one({'id':m.chat.id}, {'$inc':{'users.'+str(m.from_user.id)+'.messages':1}})
    mat=['хуй', 'пизда', 'пидор', 'мудак', 'залупа', 'блядь', 'блять', 'хуе', 'хуя', 'манда', 'еблан', 'ебан', 'пидр','даун', 'бля']
    x=0
    for ids in mat:
       if ids in m.text.lower():
          x=1
    if x==1:
+      chats.update_one({'id':m.chat.id}, {'$inc':{'users.'+str(m.from_user.id)+'.mats':1}})
       texts=['Не ругайся!','Плохо таким быть!', 'У нас здесь не матерятся!', 'Тут приличное общество, не ругайся.', 'Зачем материться?', 'Не надо так.', 'Эй, не матерись!', 'Давай без мата.']
       text=random.choice(texts)
       bot.reply_to(m, text)
 
 
+def createuser(id, username, name):
+   return {'id':id,
+           'username':username,
+           'name':name,
+           'mats':0,
+           'messages':0
+          }
+
+def createchat(id):
+   return {'id':id,
+           'users':{}
+         
+          }
 
 
 if True:
