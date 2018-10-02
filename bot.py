6ade416bc8bd66e4011dca718620f247ca26c320
egor5q <31898567+egor5q@users.noreply.github.com>
2018-10-02 10:52:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,14 +34,53 @@ def newpass(m):
          x=m.text.split('/newpass ')
          if len(x)==2:
             psw.insert_one(createpass(x[1]))
-            bot.send_message(m.chat.id, 'Пароль "'+x[1]+'" успешно создан! Всего паролей: '+str(z)
+            bot.send_message(m.chat.id, 'Пароль "'+x[1]+'" успешно создан! Всего паролей: '+str(z))
 
+@bot.message_handler(commands=['login'])
+def login(m):
+   x=m.text.split('/login ')
+   if len(x)==2:
+     pw=x[1]
+     x=psw.find({})
+     team=None
+     for ids in x:
+      if ids['password']==pw:
+         team=ids
+     if team!=None:
+         psw.update_one({'password':pw},{'$push':{'ids':m.from_user.id}})
+
+            
+            
+@bot.message_handler(commands=['start'])
+def start(m):
+ if m.chat.id==m.from_user.id:
+   x=psw.find({})
+   yes=0
+   for ids in x:
+      if m.from_user.id in ids['ids']:
+         yes=1
+   if yes==0:
+      bot.send_message(m.chat.id,'Для авторизации введите следующий текст:\n'+
+                       '/login *пароль*\nГде *пароль* - выданный вашей команде пароль.')
+  
+                             
+                             
 def createpass(p):
    return {'password':p,
           'ids':[],
           'points':0,
-          'boughted':[]}
-  
+          'boughted':[]
+          } 
+
+helplist={
+   1:'',
+   2:'',
+   3:'',
+   4:'',
+   5:''
+   }
+                             
+                             
 
 if True:
    print('7777')
